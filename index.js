c22d05c15cd8d0df73f63e2f32f3c532b71828f4
daxgdev <faniel5431@gmail.com>
2024-08-24 20:11:48 -0500

    funcionalidad para encriptar texto

---
+++ index.js
@@ -0,0 +1,26 @@
+import { $ } from './utils.js'
+
+const textField = $('.first-field > textarea')
+const resultField = $('.second-field > textarea')
+
+const rules = {
+  a: 'ai',
+  e: 'enter',
+  i: 'imes',
+  o: 'ober',
+  u: 'ufat'
+}
+const encrypt = (text) => {
+  const split = text.split('')
+  split.forEach((letter, i) => {
+    if (Object.hasOwn(rules, letter)) {
+      split[i] = rules[letter]
+    }
+  })
+
+  return split.join('')
+}
+
+textField.oninput = (e) => {
+  resultField.value = encrypt(e.target.value)
+}
