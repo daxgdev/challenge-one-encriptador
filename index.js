208ef7ded6cedc14e0a5d4dd01437e9ceebf3885
daxgdev <faniel5431@gmail.com>
2024-08-25 20:00:46 -0500

    funcionalidad para desencriptar texto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { $ } from './utils.js'
 
 const firstField = $('.first-field')
+const secondField = $('.second-field')
 const textField = $('.first-field > textarea')
 const resultField = $('.second-field > textarea')
 
@@ -11,6 +12,7 @@ const rules = {
   o: 'ober',
   u: 'ufat'
 }
+
 const encrypt = (text) => {
   const split = text.split('')
   split.forEach((letter, i) => {
@@ -22,9 +24,13 @@ const encrypt = (text) => {
   return split.join('')
 }
 
-// const decrypt = (text) => {
+const decrypt = (text) => {
+  let result = text
+  for (const rule in rules)
+    result = result.replace(new RegExp(rules[rule], 'g'), rule)
 
-// }
+  return result
+}
 
 textField.oninput = () => {
   const conditions = /[^a-z0-9 ]/g
@@ -33,12 +39,16 @@ textField.oninput = () => {
   if (firstField.classList.contains('encrypt')) {
     resultField.value = encrypt(textField.value)
   } else if (firstField.classList.contains('decrypt')) {
-    // resultField.value = decrypt(textField.value)
+    resultField.value = decrypt(textField.value)
   }
 }
 
-// document.addEventListener('click', (e) => {
-//   if (e.target.matches('.switch')) {
-    
-//   }
-// })
+document.addEventListener('click', (e) => {
+  if (e.target.matches('.switch')) {
+    e.target.classList.toggle('spin')
+    firstField.classList.toggle('encrypt')
+    firstField.classList.toggle('decrypt')
+    firstField.classList.toggle('change-function')
+    secondField.classList.toggle('change-function')
+  }
+})
