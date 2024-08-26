99a0c8b292da3aa30008c8f40c288568e6c98257
daxgdev <faniel5431@gmail.com>
2024-08-25 21:10:53 -0500

    corrección en módulo speak

--- modules/speak.js
+++ modules/speak.js
@@ -1,8 +1,10 @@
 export const speak = (text, el) => {
   const synth = window.speechSynthesis
   const utterance = new SpeechSynthesisUtterance(text)
-  synth.speak(utterance)
 
-  el.setAttribute('disabled', '')
-  utterance.onend = () => el.removeAttribute("disabled")
+  if (text) {
+    synth.speak(utterance)
+    el.setAttribute('disabled', '')
+    utterance.onend = () => el.removeAttribute('disabled')
+  }
 }
